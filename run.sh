4e1a2041c62c0e9f90ec877ae8b5c48e6b8436ee
fteufel <felix.teufel@gmail.com>
2020-10-08 09:36:34 +0200

    update run script

---
+++ run.sh
@@ -0,0 +1,28 @@
+export WANDB_PROJECT=finetune-bert
+wandb login "3850816f07ddd291761dba5da24453c2350b898a"
+export TPU_IP_ADDRESS=10.116.37.234
+export XRT_TPU_CONFIG="tpu_worker;0;$TPU_IP_ADDRESS:8470"
+
+
+
+export TRAIN_FILE=preprocessed_all_short.txt
+
+python3 train_mlm.py \
+ --output_dir=output3\
+ --model_type=bert\
+ --model_name_or_path="Rostlab/prot_bert"\
+ --mlm\
+ --do_train\
+ --train_data_file=$TRAIN_FILE \
+ --run_name=debug_train_script \
+ --mlm_probability=0.15 \
+ --block_size=512 \
+ --per_device_train_batch_size=30 \
+ --learning_rate=0.002 \
+ --weight_decay=0.01 \
+ --max_steps=10000 \
+ --line_by_line \
+ --save_steps=1000 \
+ --warmup_steps=1333 \
+ --logging_steps=10\
+ --logging_dir=firstrun\
